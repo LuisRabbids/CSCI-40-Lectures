1f59c6e9540c951c1681513eae9fd6ae7ccc320c
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-20 14:13:40 +0800

    before models

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import task_list, add_task, completed_tasks
+
+urlpatterns = [
+    path('', task_list, name="task-list"),  # /tasks/
+    path('add/', add_task, name="add-task"),  # /tasks/add/
+    path('completed/', completed_tasks, name="completed-tasks"),  # /tasks/completed/
+]
+
+app_name = 'tasks'
---
+++ tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def task_list(request):
+    ctx = {
+        "tasks": [
+        "task 1",
+        "task 2",
+        "task 3",
+        "task 4"
+        ]
+    }
+    return render(request, "tasks/task_list.html", ctx)
+
+def add_task(request):
+    return render(request, "tasks/add_task.html")
+
+def completed_tasks(request):
+    ctx = {"completed_tasks": ["Submit report", "Attend meeting"]}
+    return render(request, "tasks/completed_tasks.html", ctx)  
