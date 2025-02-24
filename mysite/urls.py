841340921efc2339b8edecd492fd304cbc9d36f2
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-24 14:42:29 +0800

    removed ledger from mysite settings and urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("ledger.urls")),
     path("tasks/", include("tasks.urls")),
     path("admin/", admin.site.urls),
 ]
