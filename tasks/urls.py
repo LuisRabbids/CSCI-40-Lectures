45be9690be7e2fb974d63483ef2c0badea6705ca
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-27 13:04:36 +0800

    Added task detail to urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import task_list, add_task, completed_tasks
+from .views import task_list, task_detail
 
 urlpatterns = [
     path('', task_list, name="task-list"),  # /tasks/
+    path('<int:id>', task_detail, name='task-detail')
 ]
 
 app_name = 'tasks'
