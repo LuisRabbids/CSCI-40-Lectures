c24f3e5d713291066c6342592b8ce554acb2cccc
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-27 11:24:14 +0800

    Added Task methods

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class TaskGroup(models.Model):
@@ -8,4 +10,18 @@ class TaskGroup(models.Model):
 class Task(models.Model):
     name = models.CharField(max_length=100)
     due_date = models.DateTimeField(null=False)
-    taskgroup = models.ForeignKey(TaskGroup, on_delete=models.CASCADE)
+    taskgroup = models.ForeignKey(
+        TaskGroup, 
+        on_delete=models.CASCADE,
+        related_name='students'
+    )
+
+    def __str__(self):
+        return '{}: due on {} unit(s)'.format(self.name, self.due_date)
+    
+    def get_absolute_url(self):
+        return reverse('task_detail', args=[str(self.name)])
+    
+    @property
+    def is_due(self):
+        return datetime.now() >= self.due_date
