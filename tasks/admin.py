c862bc65688d0ab20142ad186803437f12b3c664
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-27 13:57:34 +0800

    Added detail view customizations to class TaskAdmin

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,8 +3,13 @@ from .models import Task, TaskGroup
 
 
 # Register your models here.
+class TaskInline(admin.TabularInline):
+    model = Task
+
+
 class TaskGroupAdmin(admin.ModelAdmin):
-    model = TaskGroup
+    # This adds a table of all related Task objects
+    inlines = [TaskInline,]
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -15,9 +20,21 @@ class TaskAdmin(admin.ModelAdmin):
 
     # Display just the name and the due date in the list
     list_display = ('name', 'due_date',)
-    
+
     # Enable filtering via the teacher's name and the number of units
     list_filter = ('due_date', )
+
+    # Organizes the fields
+    fieldsets = [
+        # fieldsets is a list of tuples where the syntax is:
+        # ('label of the field', {'fields': [<list of fields>]})
+        ('Details', {
+            'fields': [
+                # the tuple puts these fields in a single line
+                ('name', 'due_date'), 'taskgroup'
+            ]
+        }),
+    ]
 
 
 # registering the model and the admin is what tells
