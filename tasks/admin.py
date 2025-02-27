88fb47940af83f0715406008508207a46fd72feb
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-27 13:53:08 +0800

    Added list view customizations to TaskAdmin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Task, TaskGroup
+
 
 # Register your models here.
+class TaskGroupAdmin(admin.ModelAdmin):
+    model = TaskGroup
+
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+
+    # Use the name to search
+    search_fields = ('name', )
+
+    # Display just the name and the due date in the list
+    list_display = ('name', 'due_date',)
+    
+    # Enable filtering via the teacher's name and the number of units
+    list_filter = ('due_date', )
+
+
+# registering the model and the admin is what tells
+# Django that admin pages must be generated for the models specified
+admin.site.register(TaskGroup, TaskGroupAdmin)
+admin.site.register(Task, TaskAdmin)
