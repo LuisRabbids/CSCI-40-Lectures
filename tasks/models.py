0ef3560cffcf360fa68e5ba78a36f76c3a7397cd
Luis Felipe <luisfelipecodes@gmail.com>
2025-02-27 11:20:24 +0800

    Added TaskGroup class and Task relationship

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=50)  
+
+
 class Task(models.Model):
     name = models.CharField(max_length=100)
     due_date = models.DateTimeField(null=False)
+    taskgroup = models.ForeignKey(TaskGroup, on_delete=models.CASCADE)
