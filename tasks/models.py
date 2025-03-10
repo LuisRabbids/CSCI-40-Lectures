9f29a91ab7d79ef73d146524772ee3bff901bf82
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-10 13:42:41 +0800

    Added a bio field for the Profile() class

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
 
 
 class TaskGroup(models.Model):
