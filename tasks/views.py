1671bddeaf4ddc9beb56b16676f4045778c06770
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-10 15:15:37 +0800

    Fixed login restriction for view functions in tasks views

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Task
 
 
+@login_required
 def task_list(request):
     tasks = Task.objects.all()
     ctx = {
@@ -11,12 +12,7 @@ def task_list(request):
     return render(request, 'tasks/task_list.html', ctx)
 
 
+@login_required
 def task_detail(request, id):
     ctx = {'task': Task.objects.get(id=id)}
     return render(request, 'tasks/task_detail.html', ctx)
-
-
-@login_required
-def view_function(request):
-    # things go here
-    pass
