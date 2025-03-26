abcbf4c1f26250e7c5099dbec407157ac1a92ea1
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-26 22:29:59 +0800

    Modified task_list() view to handle POST request

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Task
+from .models import Task, TaskGroup
 
 
 @login_required
 def task_list(request):
     tasks = Task.objects.all()
-    ctx = {
-        'tasks': tasks
-    }
-    return render(request, 'tasks/task_list.html', ctx)
+    taskgroups = TaskGroup.objects.all()
+    ctx = {"tasks": tasks, "taskgroups": taskgroups}
+    if (request.method == "POST"):
+        t = Task()
+        t.name = request.POST.get('task_name')
+        t.due_date = request.POST.get('task_due')
+        t.taskgroup = TaskGroup.objects.get(pk=request.POST.get('taskgroup'))
+        t.save()
+        return render(request, 'task_list.html', ctx)
+    else:
+        return render(request, 'task_list.html', ctx)
 
 
 @login_required
