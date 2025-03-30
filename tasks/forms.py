2e7fceab2619a33493299d82cf80b54df8e85877
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-30 21:21:54 +0800

    Removed the second TaskForm() class in forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,11 +6,3 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
-
-
-class TaskForm(forms.Form):
-    name = forms.CharField(label='Task Name', max_length=100)
-    due_date = forms.CharField(label='Task Due Date')
-    taskgroup = forms.ModelChoiceField(
-        label='Task Group', queryset=TaskGroup.objects.all()
-    )
