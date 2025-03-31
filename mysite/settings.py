0fbd205a9b1b29ddf5745af00abeeab8682752b4
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-31 12:57:03 +0800

    Added media/images folder, and MEDIA_ROOT &  MEDIA_URL in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,13 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 
+
+# Media
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
